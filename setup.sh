b7ebf5849a4bef96be23e330e95881cce644702f
Jon Martin <jonnitram@JonNitram.local>
2019-06-26 16:36:44 -0500

    added requirements.txt so can uncomment pip install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# python3.7 -m pip install -r requirements.txt
-# uncomment when requirements.txt is done
+python3.7 -m pip install -r requirements.txt
 read -p $'Does a \'tokens.txt\' file already exist? [y/n] ' tokensexist
 if [ $tokensexist = 'y' ]; then
     read -p $'Would you like to delete the old \'tokens.txt\' file and make a new one? [y/n] ' resettokens
