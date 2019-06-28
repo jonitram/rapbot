0a3c1d5d54d92149c719734d59f26cecd5c977af
Jonathon Martin <33382069+jonitram@users.noreply.github.com>
2019-06-27 22:21:08 -0500

    skeletal implementation of most commands

--- rapbot.py
+++ rapbot.py
@@ -17,7 +17,41 @@ def setup_tokens(filename):
 
 @bot.command()
 async def rap(ctx, *args):
+    # multiprocess -> spin up background generate raps
+    # store reference for potentially killing later
     return
+
+# @bot.command()
+# async def kill(ctx):
+    # if raps process reference != None
+    #   kill it
+    # else
+    #   let user know nothing was being generated
+    # return
+    
+# @bot.command()
+# async def say(ctx):
+    # if check precons:
+        # (check precons -> user in voice channel, raps file exists, nothing currently playing -> voice reference == None)
+        # set voice reference
+        # connect
+        # play
+        # stop
+        # disconnect
+    # else
+        # let user know what precon they failed
+    # return
+
+# should be awaited
+# @bot.command()
+# async def stop(ctx):
+#     if voice reference != None:
+#         (follow sbb logic)
+#         voice reference stop
+#         disconnect
+#         voice reference = None
+#     else
+#         nothing was playing
 
 def main():
     setup_tokens(tokensfile)
