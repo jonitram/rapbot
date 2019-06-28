7ed60db3ba50b1bea1ffa51c91d650be8b906b6b
Jonathon Martin <33382069+jonitram@users.noreply.github.com>
2019-06-27 22:08:48 -0500

    removed unnecessary code

--- rapbot.py
+++ rapbot.py
@@ -3,15 +3,8 @@ import asyncio
 import discord
 from discord.ext import commands
 
-# TODO:
-# just copied over sbb logic but
-# should use discord api built in command feature
-# instead of manually filtering for command_prefix
-
 tokensfile = 'tokens.txt'
 discord_token = None
-
-# client = discord.Client()
 
 bot = commands.Bot(command_prefix='.rb ')
 
@@ -24,28 +17,10 @@ def setup_tokens(filename):
 
 @bot.command()
 async def rap(ctx, *args):
-    await ctx.send('-'.join(args))
     return
-
-# @client.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(client.user.name)
-#     print(client.user.id)
-#     print('------')
-
-# @client.event
-# async1 def on_message(message):
-    # if message.content.startswith(command_prefix):
-        # do shit
-        # this is old way of filtering but should really
-        # just use discord command feature instead
-        # will have to look into that
-    # return  
 
 def main():
     setup_tokens(tokensfile)
     bot.run(discord_token)
-    # client.run(discord_token)
 
 if __name__ == "__main__": main()
